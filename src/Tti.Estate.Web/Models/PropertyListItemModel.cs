e959b7b1cb3538f6491edb9dbbff2ca1467e5be1
Dmitry Bogatykh <bogatykh@gmail.com>
2019-01-26 14:01:53 +0300

    Status for property and transaction list.

--- src/Tti.Estate.Web/Models/PropertyListItemModel.cs
+++ src/Tti.Estate.Web/Models/PropertyListItemModel.cs
@@ -6,6 +6,8 @@ namespace Tti.Estate.Web.Models
     {
         public long Id { get; set; }
 
+        public PropertyStatusModel Status { get; set; }
+
         public long Price { get; set; }
 
         public decimal Area { get; set; }
