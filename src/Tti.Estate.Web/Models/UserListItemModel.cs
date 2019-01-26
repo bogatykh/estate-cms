593721f186ba5f77f87f28607487c8246fcebf6e
Dmitry Bogatykh <bogatykh@gmail.com>
2019-01-26 14:19:15 +0300

    User list status badge.

--- src/Tti.Estate.Web/Models/UserListItemModel.cs
+++ src/Tti.Estate.Web/Models/UserListItemModel.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Tti.Estate.Web.Models
+﻿namespace Tti.Estate.Web.Models
 {
     public class UserListItemModel
     {
         public long Id { get; set; }
+
+        public UserStatusModel Status { get; set; }
 
         public string UserName { get; set; }
 
