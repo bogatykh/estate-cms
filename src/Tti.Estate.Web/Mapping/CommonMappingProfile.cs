4ce4930d79271e9708ecc4e385e74636f1362bb1
Dmitry Bogatykh <bogatykh@gmail.com>
2019-01-27 15:57:04 +0300

    dataTables remvoed.

--- src/Tti.Estate.Web/Mapping/CommonMappingProfile.cs
+++ src/Tti.Estate.Web/Mapping/CommonMappingProfile.cs
@@ -7,14 +7,7 @@ namespace Tti.Estate.Web.Mapping
     {
         public CommonMappingProfile()
         {
-            //CreateMap<IPagedResult, DataTableModel>().
-            //    ForMember(x => x.FilteredItems, x => x.MapFrom(y => y.TotalItems)).
-            //    ForMember(x => x.DrawCounter, x => x.Ignore());
-
             //CreateMap<IPagedResult, PagedResultModel>();
-
-            //CreateMap(typeof(IPagedResult<>), typeof(DataTableModel<>)).
-            //    IncludeBase(typeof(IPagedResult), typeof(DataTableModel));
 
             //CreateMap(typeof(IPagedResult<>), typeof(PagedResultModel<>)).
             //    IncludeBase(typeof(IPagedResult), typeof(PagedResultModel));
