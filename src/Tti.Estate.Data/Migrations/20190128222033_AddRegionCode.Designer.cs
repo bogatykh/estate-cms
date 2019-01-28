2129c2511c8c91ae8eb92d81f50b24b69881240d
Dmitry Bogatykh <bogatykh@gmail.com>
2019-01-29 01:20:51 +0300

    Region code migrations.

--- src/Tti.Estate.Data/Migrations/20190128222033_AddRegionCode.Designer.cs
+++ src/Tti.Estate.Data/Migrations/20190128222033_AddRegionCode.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tti.Estate.Data;
 
 namespace Tti.Estate.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190128222033_AddRegionCode")]
+    partial class AddRegionCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Tti.Estate.Data/Migrations/20190128222033_AddRegionCode.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tti.Estate.Data.Migrations
+{
+    public partial class AddRegionCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Region",
+                fixedLength: true,
+                maxLength: 7,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Region");
+        }
+    }
+}
